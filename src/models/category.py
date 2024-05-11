76e15381c46d5943dc1d063d439c58e42bcc1a94
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-11 14:41:05 +0500

    language add

--- src/models/category.py
+++ src/models/category.py
@@ -1,12 +1,11 @@
 # Your models for the app
 from sqlalchemy import Boolean, Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy_mptt.mixins import BaseNestedSets
 
 from src.models.base import BaseModel
 
 
-class Category(BaseModel, BaseNestedSets):
+class Category(BaseModel):
     __tablename__ = "category"
     name = Column(String(255))
     is_active = Column(Boolean(), default=True)
