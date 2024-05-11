dcb78a7514ee881d266fdd91aef807c814a66298
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-11 16:45:35 +0500

    salom

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,5 +1,6 @@
 from aiogram import Dispatcher
 
+from .common import router as home_router
 from .order import router as order_routes
 from .settings import router as settings_router
 from .start import router as start_routes
@@ -7,6 +8,7 @@ from .support import router as support_router
 
 
 def register_routes(dp: Dispatcher):
+    dp.include_router(home_router)
     dp.include_router(start_routes)
     dp.include_router(order_routes)
     dp.include_router(support_router)
