0639ab2e9b92f5f725e00b5bcc64ff6237b65ce1
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-11 13:12:23 +0500

    salom

--- main.py
+++ main.py
@@ -1,7 +1,77 @@
+import json
+from fastapi import Request
+from aiogram.fsm.context import FSMContext
+import logging
+from contextlib import asynccontextmanager
+
+import uvicorn
+from aiogram import Bot, Dispatcher, F, Router, types
+from aiogram.filters.callback_data import CallbackData
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import CallbackQuery, Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from fastapi import FastAPI
 
-from src.admin import app as a_router
+from src import load_config
+from src.handlers import register_routes
+from src.handlers.keyboards import view_button
+from src.middlewares.config import ConfigMiddleware
 
-app = FastAPI()
+router = Router()
 
-app.mount("/", a_router)
+logger = logging.getLogger(__name__)
+
+config = load_config(".env")
+
+WEBHOOK_PATH = f"/bot/{config.tg.token}"
+WEBHOOK_URL = config.tg.webhook_url + WEBHOOK_PATH
+
+storage = MemoryStorage()
+
+bot = Bot(token=config.tg.token, parse_mode="HTML")
+dp = Dispatcher(storage=storage)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await bot.set_webhook(url=WEBHOOK_URL)
+
+    yield
+    await bot.delete_webhook()
+
+
+app = FastAPI(lifespan=lifespan)
+
+# Register middlewares
+dp.update.middleware(ConfigMiddleware(config))
+
+# Register routes
+register_routes(dp)
+
+
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(update: dict):
+
+    telegram_update = types.Update(**update)
+    await dp.feed_update(bot=bot, update=telegram_update)
+
+
+class MyCallback(CallbackData, prefix="my"):
+    foo: str
+    bar: int
+
+
+@app.post('/send-message/')
+async def bot_webhook(msg: str, user_id: str, order_id: int):
+
+    await bot.send_message(user_id, msg)
+    # await state.set_state("dwadaw")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+
+    # uvicorn.run("app:app", host="0.0.0.0", port=8000,reload=True)
--- src/config.py
+++ src/config.py
@@ -1,12 +1,15 @@
 from decouple import config
 
-SECRET_KEY = config('JWT_SECRET_KEY', "")
-REFRESH_SECRET_KEY = config('JWT_REFRESH_SECRET_KEY')
-DB_NAME = config('DB_NAME', 'test')
-DB_HOST = config('DB_HOST', 'localhost')
-DB_PORT = config('DB_PORT', 3306)
-DB_PASSWORD = config('DB_PASSWORD', '11111111')
-DB_USER = config('DB_USER', 'root')
-DB_DIALECT = config('DB_DIALECT', "mysql+mysqldb")
-DATABASE_URL = f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-UPLOAD_DIRECTORY = config("UPLOAD_DIRECTORY", "upload")
+# SECRET_KEY = config('JWT_SECRET_KEY', "")
+# REFRESH_SECRET_KEY = config('JWT_REFRESH_SECRET_KEY')
+# DB_NAME = config('DB_NAME', 'test')
+# DB_HOST = config('DB_HOST', 'localhost')
+# DB_PORT = config('DB_PORT', 3306)
+# DB_PASSWORD = config('DB_PASSWORD', '11111111')
+# DB_USER = config('DB_USER', 'root')
+# DB_DIALECT = config('DB_DIALECT', "mysql+mysqldb")
+# DATABASE_URL = f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# UPLOAD_DIRECTORY = config("UPLOAD_DIRECTORY", "upload")
+
+TOKEN = config("TOKEN")
+WEBHOOK_URL = config("WEBHOOK_URL")
