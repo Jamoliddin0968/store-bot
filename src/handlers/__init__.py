2d5460f4343179c183bd849a840529eab5e6c3bc
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-11 16:41:49 +0500

    add home menu

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher
 
 from .order import router as order_routes
+from .settings import router as settings_router
 from .start import router as start_routes
 from .support import router as support_router
 
@@ -9,3 +10,4 @@ def register_routes(dp: Dispatcher):
     dp.include_router(start_routes)
     dp.include_router(order_routes)
     dp.include_router(support_router)
+    dp.include_router(settings_router)
