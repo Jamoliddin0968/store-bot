97f4a64e54c94d1dca49c33debe9d59aa59d908d
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-11 16:25:43 +0500

    add support

--- src/handlers/support.py
+++ src/handlers/support.py
@@ -21,7 +21,8 @@ dp = Dispatcher()
 
 @router.message(F.text == "Biz bilan bo'g'lanish")
 async def start_handler(message: types.Message):
-    await message.answer(f"Assalomu alaykum {message.from_user.first_name}")
+    await message.answer("""Biz bilan bog'lanish\n+998902720884\n+998997600884"""
+                         )
     description = "Assalomu alaykum ! YotiqTut onlayn magazinga xush kelibsiz.\n"
     description += "Привет! Добро пожаловать в интернет-журнал YatiqTut.\n"
     description += "Hello! Welcome to the YatiqTut online store\n"
