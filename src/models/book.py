36f7a584c5f8651d0d884621addc074d3a86d7c1
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-11 14:36:14 +0500

    language add

--- src/models/book.py
+++ src/models/book.py
@@ -1,6 +1,4 @@
-# Your models for the app
-from bson import ObjectId
-from mongoengine import Document, StringField
+
 from sqlalchemy import Column, String
 
 from src.models.base import BaseModel
@@ -10,10 +8,3 @@ class Book(BaseModel):
     __tablename__ = "books"
     name = Column(String(127))
     description = Column(String(255))
-
-
-class BookMongo(Document):
-    id = ObjectId()
-    meta = {'collection': 'books'}
-    name = StringField(max_length=127, required=True)
-    description = StringField(max_length=255)
