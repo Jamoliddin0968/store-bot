d2bbe2743de9bc0a82cc51640b39eefdd1b3c1f0
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-16 19:05:28 +0500

    add new bug

--- src/handlers/order.py
+++ src/handlers/order.py
@@ -71,7 +71,7 @@ async def get_subcategories(callback: CallbackQuery):
         category_id = subcategories[0].subcategory_id
         lst = [InputMediaPhoto(media=FSInputFile(item.image))
                for item in subcategories]
-        await callback.message.answer_media_group(photo=FSInputFile(subcategories[0].image))
+        await callback.message.answer_media_group(media=lst)
         await callback.message.answer(text="Mahsulotni tanlang", reply_markup=create_inline_buttons(prefix="product_", data=subcategories, return_prefix=f"subcategory_{subcategory_id}"))
         await callback.message.delete()
 
